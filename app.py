d4f1c61920d8059df71d0e2ac8026d938163e0b7
puerdon <dondon726@gmail.com>
2022-05-11 21:46:43 +0800

    update

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ import base64
 app = Flask(__name__)
 CORS(app)
 
+# 檢查有沒有data資料夾
+if not os.path.isdir('./data'):
+    os.mkdir('./data')
+
 @app.route("/save_data", methods=['POST'])
 def save_data():
 
     if request.json is not None:
-        print('@@@@')
-        random_number_id = request.json['random_number_id']
 
+        random_number_id = request.json['random_number_id']
 
         # 如果是得到 survey 的
         if request.json["trial_type"] == "survey": 
@@ -24,11 +27,7 @@ def save_data():
         # 如果是得到 html-audio-response 的
         elif request.json["trial_type"] == "html-audio-response": 
                 audio_base64_string = request.json['response']
-                print(audio_base64_string)
                 audiofile = base64.b64decode(bytes(audio_base64_string, 'utf-8'))
-
-                print('cccccc')
-                print(audiofile)
 
                 with open(f'./data/{random_number_id}/x.wav', 'wb') as f:
                     f.write(audiofile)
